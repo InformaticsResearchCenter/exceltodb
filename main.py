f80ed265ba2741d938448861a424d1af7dd4fef5
DindaMajesty <dindamesti@gmail.com>
2020-03-31 08:53:46 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import exceltodb
+
+df=exceltodb.openFile()
+if df.index.stop == exceltodb.matchTrxwithVA(df, trxidcolumn='trx_id', virtualaccountcolumn='virtual_account'):
+    df=exceltodb.removeUnname(df)
+    exceltodb.setDefaultEmail(df, 'customer_email')
+    exceltodb.setTrxID(df)
+    exceltodb.setExpiredDate(df, 'expired_date')
+    exceltodb.setExpiredTime(df, 'expired_time')
+    exceltodb.setTrxAmount(df, 'trx_amount')
+    exceltodb.dataframeToDB(df)
+else:
+    print('DATA NOT MATCH')
