2700718de3cbca633fd06398c42c9ee0a6f32c1f
DindaMajesty <dindamesti@gmail.com>
2020-03-31 08:59:48 +0700

    parameter add

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ df=exceltodb.openFile()
 if df.index.stop == exceltodb.matchTrxwithVA(df, trxidcolumn='trx_id', virtualaccountcolumn='virtual_account'):
     df=exceltodb.removeUnname(df)
     exceltodb.setDefaultEmail(df, 'customer_email')
-    exceltodb.setTrxID(df)
+    exceltodb.setTrxID(df, 'trx_id')
     exceltodb.setExpiredDate(df, 'expired_date')
     exceltodb.setExpiredTime(df, 'expired_time')
     exceltodb.setTrxAmount(df, 'trx_amount')
