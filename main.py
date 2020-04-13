77e6e57e1a712aef97fc5d89589bd41aa5a9cac4
DindaMajesty <dindamesti@gmail.com>
2020-04-13 13:22:02 +0700

    initial new commit

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
-import exceltodb
+import pandas as pd
+import pymysql, config
 
-df=exceltodb.openFile()
-if df.index.stop == exceltodb.matchTrxwithVA(df, trxidcolumn='trx_id', virtualaccountcolumn='virtual_account'):
-    df=exceltodb.removeUnname(df)
-    exceltodb.setDefaultEmail(df, 'customer_email')
-    exceltodb.setTrxID(df, 'trx_id')
-    exceltodb.setExpiredDate(df, 'expired_date')
-    exceltodb.setExpiredTime(df, 'expired_time')
-    exceltodb.setTrxAmount(df, 'trx_amount')
-    exceltodb.dataframeToDB(df)
-else:
-    print('DATA NOT MATCH')
+def dbConnectSiap():
+    db=pymysql.connect(config.db_host_siap, config.db_username_siap, config.db_password_siap, config.db_name_siap)
+    return db
+
+def openFile():
+    dataframe = pd.read_excel(io='va.xlsx', sheet_name=0)
+    pd.set_option('display.max_columns', 5)
+    return dataframe
+
+def getStudentData(studentid):
+    db=dbConnectSiap()
+    sql='select Nama, HandphoneOrtu, EmailOrtu from simak_mst_mahasiswa where MhswID={studentid}'.format(studentid=studentid)
+    with db:
+        cur=db.cursor()
+        cur.execute(sql)
+        rows=cur.fetchone()
+        if rows is not None:
+            ret=rows
+        else:
+            ret=''
+    return ret
+
+def cekNumber(row):
+    studentid=row['trx_id'].split("-")[2]
+    print('On Proccess: {studentid}'.format(studentid=studentid))
+    data=getStudentData(studentid=studentid)
+    if row['customer_phone']==data[1]:
+        ret=True
+    else:
+        ret=False
+    return ret
+
+def setNumber(row):
+    studentid = row['trx_id'].split("-")[2]
+    print('On Proccess Set Phone Number: {studentid}'.format(studentid=studentid))
+    data=getStudentData(studentid=studentid)
+    return data[1]
+
+df=openFile()
+df['isPhoneNumber']=df.apply(cekNumber, axis=1)
+phonetrue=df.loc[df['isPhoneNumber']==True]
+phonefalse=df.loc[df['isPhoneNumber']==False]
+phonefalse['customer_phone']=df.apply(setNumber, axis=1)
+df_fix=pd.concat([phonetrue, phonefalse])
+pd.set_option('display.max_columns', 3000)
+df_fix['isPhoneNumber']=df_fix.apply(cekNumber, axis=1)
+truekek=df_fix.loc[df_fix['isPhoneNumber']==True]
+print(len(truekek))
